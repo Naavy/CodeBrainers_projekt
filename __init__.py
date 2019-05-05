4c8c46e95c7b391b3c9062d3e1899a74a80d20e3
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-05-05 19:26:52 +0200

    Added Flask-Admin

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from playhouse.flask_utils import FlaskDB
+from flask_admin import Admin
 from .config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db_wrapper = FlaskDB(app)
+admin = Admin(app, name='Projekt bazowy', template_mode='bootstrap3')
 
 from . import views, models
