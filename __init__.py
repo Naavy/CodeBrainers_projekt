62f8a074df13d3b1cb61aa35883b648ae8236034
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-05-05 16:00:29 +0200

    Basic Flask project boilerplate

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+from .config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+# migrate = Migrate(db, app)
+
+from . import views, models
